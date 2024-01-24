38d955bc7074d2a4aef8dd839b33221eb98348e9
Tabish-yaseen <tabishmir36@gmail.com>
2024-01-24 20:43:47 +0530

    getting expenses on the basis of filter

--- src/App.js
+++ src/App.js
@@ -1,58 +1,60 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react';
 import './App.css';
-import ExpenseItem  from './components/Expenses/ExpenseItem';
+import ExpenseList from './components/Expenses/ExpenseList';
 import ExpenseFilter from './components/Expenses/ExpenseFilter';
-import Cards from './components/UI/Cards'
+import Cards from './components/UI/Cards';
 import NewExpense from './components/NewExpense/NewExpense';
 
+const previousExpenses = [
+  { id: '1', title: 'food', Amount: 100, time: new Date(2025, 0, 20), location: 'delhi' },
+  { id: '2', title: 'petrol', Amount: 200, time: new Date(2024, 0, 21), location: 'mumbai' },
+  { id: '3', title: 'cloth', Amount: 500, time: new Date(2025, 0, 22), location: 'srinagar' },
+  { id: '4', title: 'books', Amount: 200, time: new Date(2024, 0, 23), location: 'amritsar' },
+];
+
 function App() {
-  const previousExpenses=[
-    {key:'1',title:'food',Amount:100,time:new Date(2024,0,20),location:'delhi'},
-    {key:'2',title:'petrol',Amount:200,time:new Date(2024,0,21),location:'mumbai'},
-    {key:'3',title:'cloth',Amount:500,time:new Date(2024,0,22),location:'srinagar'},
-    {key:'4',title:'books',Amount:200,time:new Date(2024,0,23),location:'amritsar'},
-  ]
-  const[updatedExpenses,setExpenses]=useState(previousExpenses)
+  const [updatedExpenses, setExpenses] = useState(previousExpenses);
+  const [selectedValue, setSelectedValue] = useState('2023');
+
 
   const addExpense=(expense)=>{
-    let newExpenses=[...updatedExpenses]
-    newExpenses.push(expense)
-    setExpenses(newExpenses)
-
-  }
-  const[selectedValue,setSelectedValue]=useState('2023')
-  const changleFilter=(value)=>{
-    setSelectedValue(value)
-
+    setExpenses((previousExpenses)=>{
+      return[expense,...previousExpenses]
+    })
 
   }
 
-  const deleteExpenses=(i)=>{
-    let newExpenses=[...updatedExpenses]
-    let xyz=newExpenses.filter((element)=>{
-      return element.key!=i
-     })
-   setExpenses(xyz)
-
-  }
+  const changleFilter = (value) => {
+    setSelectedValue(value);
+    setExpenses(previousExpenses.filter((expense) => {
+      return expense.time.getFullYear() === parseInt(value);
+    }));
+  };
+  const deleteExpenses = (key) => {
+    setExpenses((prevExpenses) => {
+      return prevExpenses.filter((expense) => expense.id !== key);
+    });
+  };
 
   return (
     <Cards>
-      <ExpenseFilter  selectedYear={selectedValue} onChangeFilter={changleFilter}/>
+      <ExpenseFilter selectedYear={selectedValue} onChangeFilter={changleFilter} />
       <NewExpense onAddExpenseHandler={addExpense} />
-      {updatedExpenses.map((element) => {
-        return (
-          <ExpenseItem
-            key={element.key}
-            title={element.title}
-            Amount={element.Amount}
-            time={element.time}
-            location={element.location}
-            index={element.key} 
-            deleteExpenses={deleteExpenses}
-          />
-        );
-      })}
+      <ExpenseList updatedExpenses={updatedExpenses} deleteExpenses={deleteExpenses}/>
+      
+      {/* {updatedExpenses.length === 0 && <p>No expenses</p>}
+       {updatedExpenses.length > 0 &&
+      updatedExpenses.map((element) => (
+        <ExpenseItem
+          key={element.id}
+          title={element.title}
+          Amount={element.Amount}
+          time={element.time}
+          location={element.location}
+          index={element.id}
+          deleteExpenses={deleteExpenses}
+        />
+      ))} */}
     </Cards>
   );
 }
